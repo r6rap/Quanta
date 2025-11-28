c110861e3ea42c2276edbbfee897709e2ba96a67
r6rap <ahmadirafif7@gmail.com>
2025-11-29 03:12:35 +0700

    modified config, create model user and setup jwt token utilities

--- config/config.go
+++ config/config.go
@@ -1,1 +1,47 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	var err error
+
+	env := godotenv.Load()
+	if env != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	// dsn, mean data source name. It is a string that contains the connection information for the database.
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+	// open a connection to postgresql
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		// logger to take notes activiy query
+		Logger: logger.Default.LogMode(logger.Info),
+		// prepare statement to improve performance
+		PrepareStmt: true,
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
+
+	log.Println("Connected to database")
+}
---
+++ internal/models/user.go
@@ -0,0 +1,24 @@
+package user
+
+import "time"
+
+type User struct {
+	ID 				uint		`gorm:"primaryKey" json:"id"`
+	Email 			string		`gorm:"unique;not null" json:"email" binding:"required"`
+	PasswordHash 	string		`gorm:"not null" json:"-"` // mean don't include json response
+	Name 			string		`gorm:"not null" json:"name" binding:"required"`
+	AvatarUrl 		*string		`json:"avatar_url,omitempty"` // mean can empty
+	CreatedAt 		time.Time	`json:"created_at"`
+	UpdatedAt 		time.Time	`json:"updated_at"`
+}
+
+type RegisterInput struct {
+	Email string `json:"email" binding:"required, email"`
+	Password string `json:"password" binding:"required, min=8"`
+	Name string `json:"name" binding:"required, min=4"`
+}
+
+type LoginInput struct {
+	Email string `json:"email" binding:"required, email"`
+	Password string `json:"password" binding:"required, min=8"`
+}
